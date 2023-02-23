d500cf9cefa2c8e10a78f201d96c69802d04f1be
Daniel Palacios <daniel80380@gmail.com>
2023-02-23 19:29:40 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,6 +1,23 @@
 import { FlatList } from "react-native";
 import React from "react";
 import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+const globalVariable = "I am a global variable (in this screen file).";
+const Users = [{
+  id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+  title: 'John Doe',
+  image: 'https://i.imgur.com/8ZfWgpy.jpg',
+  message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+}, {
+  id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+  title: 'Mary Jane',
+  image: 'https://i.imgur.com/BBLwOVm.png',
+  message: 'Nam eleifend tellus dapibus, mollis orci, elementum enim.'
+}, {
+  id: '58694a0f-3da1-471f-bd96-145571e29d72',
+  title: 'Sean Paul',
+  image: 'https://i.imgur.com/MIapmvB.png',
+  message: 'Duis eget augue ornare, dignissim quam, consequat tellus.'
+}];
 
 const Untitled3 = () => {
   return <SafeAreaView style={styles.safeArea}>
