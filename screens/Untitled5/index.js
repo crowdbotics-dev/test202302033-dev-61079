8183e7ea84a315a47f78d617b7d13e082a7fb985
Daniel Palacios <daniel80380@gmail.com>
2023-02-28 17:09:52 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled5/index.js
+++ screens/Untitled5/index.js
@@ -1,7 +1,14 @@
+import { slice } from "./../Untitled2/sliceModels.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled5 = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(slice.actions.api_v1_signup_create());
+  }, []);
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
