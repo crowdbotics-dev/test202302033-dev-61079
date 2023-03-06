ce1586eaa1fcb3efa90bd553a60ec55a7cdada7e
Daniel Palacios <daniel80380@gmail.com>
2023-03-06 09:38:54 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled4/index.js
+++ screens/Untitled4/index.js
@@ -1,3 +1,5 @@
+import { FlatList } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +10,29 @@ const Untitled4 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><FlatList style={styles.CGAvzULe} renderItem={({
+        item
+      }) => <View style={styles.JvVeUKIz}></View>} ItemSeparatorComponent={() => <View style={styles.mrzAujdR} />} data={[1, 2, 3]} keyExtractor={(item, index) => index}></FlatList></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  CGAvzULe: {
+    position: "absolute",
+    width: 100,
+    height: 150
+  },
+  JvVeUKIz: {
+    width: "100%",
+    height: 60,
+    backgroundColor: "#FFFFFF"
+  },
+  mrzAujdR: {
+    backgroundColor: "#000000",
+    height: 1
   }
 });
 export default Untitled4;
